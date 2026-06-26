0f06106ff0921e0b314115c32b888ad1171f9962
Seemab Mehmood <55745510+Seemab-Mehmood@users.noreply.github.com>
2026-06-26 08:53:47 +0500

    Update app.js

--- y4y-directory/public/js/app.js
+++ y4y-directory/public/js/app.js
@@ -75,7 +75,7 @@ function switchView(view) {
     loadInvitations();
   } else if (view === "empower") {
     loadEmpowerStats();
-  } else if (view === "view-pledge-details") {
+  } else if (view === "pledge-details") {
     // Custom routines for pledge details initialization can be declared here if needed
   }
 }
